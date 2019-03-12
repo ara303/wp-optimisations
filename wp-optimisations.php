a024b31be9f9d984e26121e4972ba8de2107a4ae
E. Adams <edadams@users.noreply.github.com>
2019-03-12 20:50:52 +0000

    Update wp-optimisations.php

--- wp-optimisations.php
+++ wp-optimisations.php
@@ -9,16 +9,26 @@
 defined( 'ABSPATH' ) or die();
 
 /**
- * Remove unused CSS and JS files.
+ * Remove unused CSS files.
  */
 function optimisations_remove_unused_css() {
     wp_dequeue_style( 'wp-block-library' );
-    
-    wp_deregister_script( 'jquery' );
-    wp_deregister_script( 'jquery-migrate' );
 }
 add_action( 'wp_enqueue_scripts', 'optimisations_remove_unused_css', 100 );
 
+/**
+ * Remove jQuery Migrate.
+ * Credit: https://www.infophilic.com/remove-jquery-migrate-wordpress/
+ */
+ function optimisations_remove_jquery_migrate( $scripts ) {
+     if( !is_admin() && isset( $scripts->registered['jquery'] ) ){
+         $script = $scripts->registered['jquery'];
+         if ( $script->deps ) {
+             $script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+         }
+     }
+ }
+ add_action( 'wp_default_scripts', 'optimisations_remove_jquery_migrate' );
 
 /**
  * Remove WP oEmbed support. 
